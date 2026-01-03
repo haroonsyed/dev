758a459552a0c745ae24741002c62a0bc8dd0db0
haroonsyed <haroonaminsyed@gmail.com>
2026-01-03 08:41:21 -0500

    Some for security hooks for k3s

--- nixos/configuration-pc.nix
+++ nixos/configuration-pc.nix
@@ -300,8 +300,12 @@
       "--kube-apiserver-arg=admission-control-config-file=/etc/rancher/k3s/server/psa.yaml"
       "--kubelet-arg=pod-max-pids=2048"
       "--kube-apiserver-arg=enable-admission-plugins=NodeRestriction"
+      "--kubelet-arg=authentication-token-webhook=true"
+      "--kubelet-arg=authorization-mode=Webhook"
       # "--kubelet-arg=tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
     ];
+    
+    # TODO: Add manifest for helm charts of argocd
   };
   environment.variables.KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
 }
